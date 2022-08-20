20bce0c52ab63434757986aab3b3b4cf1f7f21e0
Shashi Kumar <kumarshashi5294@gmail.com>
2022-08-20 16:55:17 +0530

    added dashboard template to view all todos

--- src/main/java/com/example/playwithsb/controllers/TodoItemController.java
+++ src/main/java/com/example/playwithsb/controllers/TodoItemController.java
@@ -1,18 +1,32 @@
 package com.example.playwithsb.controllers;
 
+import com.example.playwithsb.repositories.TodoItemRepository;
+import java.time.Instant;
+import java.time.ZoneId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 @Controller
 public class TodoItemController {
-    private final Logger log=LoggerFactory.getLogger(TodoItemController.class);
-    @GetMapping("/")
-    public ModelAndView index() {
-        log.debug("Request to Get Index");
-        ModelAndView modelAndView= new ModelAndView("index");
-        return modelAndView;
-    }
-    
+  private final Logger log = LoggerFactory.getLogger(TodoItemController.class);
+
+  @Autowired
+  private TodoItemRepository todoItemRepository;
+
+  @GetMapping("/")
+  public ModelAndView index() {
+    log.debug("Request to Get Index");
+    ModelAndView modelAndView = new ModelAndView("index");
+    modelAndView.addObject(
+      "today",
+      Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek()
+    );
+
+    modelAndView.addObject("todoItems", todoItemRepository.findAll());
+    return modelAndView;
+  }
 }
