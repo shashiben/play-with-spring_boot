80cc782981f4de9b88e44ac0c9b7a01c694a71cc
Shashi Kumar <kumarshashi5294@gmail.com>
2022-08-20 15:58:25 +0530

    initial commit

---
+++ src/test/java/com/example/playwithsb/PlayWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playwithsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
